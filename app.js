6207c2d191fc2ee5cf185e4fa16b5521f249c5eb
Antika Noor <antika.noor@bjitacademy.com>
2023-08-25 17:08:27 +0600

    all 5 functions implemented

--- app.js
+++ app.js
@@ -1,143 +1,154 @@
-// const Product = require("./product")
 const http = require("http")
 const { success, failure } = require("./utils/success-error")
 const Products = require("./model/products")
 const path = require("path")
-// const { fail } = require("assert")
-// const product = require("./product")
 
 const server = http.createServer(async (request, response) => {
-
-  const getQueryParams = () => {
-    if (request.url.startsWith("/products/detail") && request.method === "GET") {
-      const urlParts = request.url.split("?")
-      if (urlParts.length < 2) {
-        return null
-      }
-
-      const params = new URLSearchParams(urlParts[1])
-      const queryParams = {}
-      for (const [param, value] of params) {
-        queryParams[param] = value
-      }
-      return queryParams
-    }
-  }
-  // const queryParams = getQueryParams()
-
-  //   if (queryParams !== null) {
-  //     const id = queryParams.id
-  //     if (id) {
-  //       // Read the JSON file and find the desired product
-  //       fs.readFile(path.join(__dirname, "data", "manga.json"), (err, data) => {
-  //         if (err) {
-  //           response.writeHead(500, { "Content-Type": "application/json" })
-  //           response.write(
-  //             JSON.stringify(failure("Internal Server Error"))
-  //           )
-  //           return response.end()
-  //         }
-
-  //         try {
-  //           const products = JSON.parse(data)
-  //           const foundProduct = products.find(item => item.id === id)
-  //           if (foundProduct) {
-  //             response.writeHead(200, { "Content-Type": "application/json" })
-  //             response.write(
-  //               JSON.stringify(success("Successfully got the product", foundProduct))
-  //             )
-  //           } else {
-  //             response.writeHead(400, { "Content-Type": "application/json" })
-  //             response.write(
-  //               JSON.stringify(failure("Failed to get the product"))
-  //             )
-  //           }
-  //           return response.end()
-  //         } catch (error) {
-  //           response.writeHead(500, { "Content-Type": "application/json" })
-  //           response.write(
-  //             JSON.stringify(failure("Internal Server Error"))
-  //           )
-  //           return response.end()
-  //         }
-  //       })
-  //     } else {
-  //       // Handle the case when "id" is missing in the query parameters
-  //       response.writeHead(400, { "Content-Type": "application/json" })
-  //       response.write(
-  //         JSON.stringify(failure("ID not found"))
-  //       )
-  //       return response.end()
-  //     }
-  //   } else {
-  //     // Handle the case when no query parameters are present
-  //     response.writeHead(400, { "Content-Type": "application/json" })
-  //     response.write(
-  //       JSON.stringify(failure("Body not found"))
-  //     )
-  //     return response.end()
-  //   }
-
-
-  // // getQueryParams()
   const requestURL = request.url.split("?")[0]
-  // // const queryURL = request.url.split("?")[1]
+  // The queryParam function
+  const getQueryParams = () => {
+    const urlParts = request.url.split("?")
+    if (urlParts.length < 2) {
+      return null
+    }
 
-  // get all 
+    // Getting the part that comes after ?
+    const params = new URLSearchParams(urlParts[1])
+    const queryParams = {}
+    for (const [param, value] of params) {
+      queryParams[param] = value
+    }
+    return queryParams
+  }
+  // calling the getQueryParams
+  const queryParams = getQueryParams()
+
+  // get all
   if (requestURL === "/manga/all" && request.method === "GET") {
     try {
       const result = await Products.getAll()
       if (result.success) {
         response.writeHead(200, { "Content-Type": "application/json" })
         response.write(
-          JSON.stringify(success("Successfully got all products", JSON.parse(result.data)))
+          JSON.stringify(
+            success("Successfully got all products", JSON.parse(result.data))
+          )
         )
         return response.end()
-      }
-      else {
+      } else {
         response.writeHead(400, { "Content-Type": "application/json" })
-        response.write(
-          JSON.stringify(failure("Failed to get products"))
-        )
+        response.write(JSON.stringify(failure("Failed to get products")))
         return response.end()
       }
-    }
-    catch (error) {
+    } catch (error) {
       // console.log(error)
       response.writeHead(500, { "Content-Type": "application/json" })
-      response.write(
-        JSON.stringify(failure("Internal Server Error"))
-      )
+      response.write(JSON.stringify(failure("Internal Server Error")))
+      return response.end()
+    }
+  }
+
+  // get by stock and price
+  if (requestURL === "/manga/stockandprice" && request.method === "GET") {
+    try {
+      const result = await Products.getByPriceAndStock(parseInt(queryParams.stock), parseFloat(queryParams.price))
+      if (result.success) {
+        response.writeHead(200, { "Content-Type": "application/json" })
+        response.write(
+          JSON.stringify(
+            success("Successfully got all products", JSON.parse(result.data))
+          )
+        )
+        return response.end()
+      } else {
+        response.writeHead(400, { "Content-Type": "application/json" })
+        response.write(JSON.stringify(failure("Failed to get products")))
+        return response.end()
+      }
+    } catch (error) {
+      // console.log(error)
+      response.writeHead(500, { "Content-Type": "application/json" })
+      response.write(JSON.stringify(failure("Internal Server Error")))
       return response.end()
     }
   }
 
   //get one by id
-  else if (requestURL === "/manga/getone" && request.method === "GET") {
+  else if (requestURL === "/manga/detail" && request.method === "GET") {
     try {
-      const result = await Products.getOneById(20)
+      const result = await Products.getOneById(parseInt(queryParams.id))
+
       if (result.success) {
         response.writeHead(200, { "Content-Type": "application/json" })
         response.write(
-          JSON.stringify(success("Successfully got the product", JSON.parse(result.data)))
+          JSON.stringify(success(result.message, JSON.parse(result.data)))
         )
         return response.end()
-      }
-      else {
+      } else {
         response.writeHead(400, { "Content-Type": "application/json" })
-        response.write(
-          JSON.stringify(failure("ID not found"))
-        )
+        response.write(JSON.stringify(failure(result.message)))
         return response.end()
       }
-    }
-    catch (error) {
-      // console.log(error)
+    } catch (error) {
       response.writeHead(500, { "Content-Type": "application/json" })
-      response.write(
-        JSON.stringify(failure("Internal Server Error"))
-      )
+      response.write(JSON.stringify(failure("Internal Server Error")))
       return response.end()
+    }
+  }
+
+  //delete one by id
+  else if (requestURL === "/manga/delete" && request.method === "DELETE") {
+    try {
+      const idToDelete = parseInt(queryParams.id)
+
+      const result = await Products.deleteOneById(idToDelete)
+      if (result.success) {
+        response.writeHead(200, { "Content-Type": "application/json" })
+        response.write(
+          JSON.stringify(success(result.message, JSON.parse(result.data)))
+        )
+        return response.end()
+      } else {
+        response.writeHead(400, { "Content-Type": "application/json" })
+        response.write(JSON.stringify(failure(result.message)))
+        return response.end()
+      }
+    } catch (error) {
+      response.writeHead(500, { "Content-Type": "application/json" })
+      response.write(JSON.stringify(failure("Internal Server Error")))
+      return response.end()
+    }
+  }
+
+  // Update something
+  else if (requestURL === "/manga/up" && request.method === "PUT") {
+    try {
+      let requestBody = ""
+
+      request.on("data", (chunk) => {
+        requestBody += chunk
+      })
+
+      request.on("end", async () => {
+        const result = await Products.updateOneById(
+          parseInt(queryParams.id),
+          JSON.parse(requestBody)
+        )
+        if (result.success) {
+          response.writeHead(200, { "Content-Type": "application/json" })
+          response.write(JSON.stringify(success(result.message)))
+        } else {
+          response.writeHead(400, { "Content-Type": "application/json" })
+          response.write(
+            JSON.stringify(failure(result.message || "Could not add the manga"))
+          )
+        }
+        response.end()
+      })
+    } catch (error) {
+      response.writeHead(500, { "Content-Type": "application/json" })
+      response.write(JSON.stringify(failure("Internal Server Error")))
+      response.end()
     }
   }
 
@@ -155,9 +166,7 @@ const server = http.createServer(async (request, response) => {
 
         if (result.success) {
           response.writeHead(200, { "Content-Type": "application/json" })
-          response.write(
-            JSON.stringify(success(result.message))
-          )
+          response.write(JSON.stringify(success(result.message)))
         } else {
           response.writeHead(400, { "Content-Type": "application/json" })
           response.write(
@@ -168,21 +177,55 @@ const server = http.createServer(async (request, response) => {
       })
     } catch (error) {
       response.writeHead(500, { "Content-Type": "application/json" })
-      response.write(
-        JSON.stringify(failure("Internal Server Error"))
-      )
+      response.write(JSON.stringify(failure("Internal Server Error")))
       response.end()
     }
   }
 
-  else {
-    response.writeHead(405, { "Content-Type": "application/json" })
-      response.write(
-        JSON.stringify(failure("There might be some problem with the method or url you used."))
-      )
+  // Sort data by id
+  else if (requestURL === "/manga/sortbyid" && request.method === "GET") {
+    try {
+      const result = await Products.sortDataById()
+      if (result.success) {
+        response.writeHead(200, { "Content-Type": "application/json" })
+        response.write(JSON.stringify(success(result.message)))
+      } else {
+        response.writeHead(400, { "Content-Type": "application/json" })
+        response.write(JSON.stringify(failure(result.message)))
+      }
       response.end()
+    } catch (error) {
+      response.writeHead(500, { "Content-Type": "application/json" })
+      response.write(JSON.stringify(failure("Internal Server Error")))
+      response.end()
+    }
   }
-
+  // Sort data by name
+  else if (requestURL === "/manga/sortbyname" && request.method === "GET") {
+    try {
+      const result = await Products.sortDataByName()
+      if (result.success) {
+        response.writeHead(200, { "Content-Type": "application/json" })
+        response.write(JSON.stringify(success(result.message)))
+      } else {
+        response.writeHead(400, { "Content-Type": "application/json" })
+        response.write(JSON.stringify(failure(result.message)))
+      }
+      response.end()
+    } catch (error) {
+      response.writeHead(500, { "Content-Type": "application/json" })
+      response.write(JSON.stringify(failure("Internal Server Error")))
+      response.end()
+    }
+  } else {
+    response.writeHead(405, { "Content-Type": "application/json" })
+    response.write(
+      JSON.stringify(
+        failure("There might be some problem with the method or url you used.")
+      )
+    )
+    response.end()
+  }
 })
 
 server.listen(8000, () => {
--- model/products.js
+++ model/products.js
@@ -3,82 +3,362 @@ const fsPromise = require("fs").promises
 const path = require("path")
 
 class Product {
+  // Getting the date
+  async getTimeFunc() {
+    const year = new Date().getFullYear()
+    const month = new Date().getMonth()
+    const day = new Date().getDay()
+    const hour = new Date().getHours()
+    const minute = new Date().getMinutes()
+    const second = new Date().getSeconds()
+    const formattedDate = `${year}-${month}-${day} ${hour}:${minute}:${second}`
+    return formattedDate
+  }
+  // Get all
+  async getAll() {
+    return fsPromise
+      .readFile(path.join(__dirname, "..", "data", "manga.json"), {
+        encoding: "utf-8",
+      })
+      .then((data) => {
+        // Just an example of destructuring
+        const dataJson = JSON.parse(data)
+        const { id, name, price, stock, author } = dataJson[0]
+        console.log("id: ", id)
+        console.log("name: ", name)
+        console.log("price: ", price)
 
-    //get all
-    async getAll() {
-        return fsPromise
-            .readFile(path.join(__dirname, "..", "data", "manga.json"), {
-                encoding: "utf-8"
-            })
-            .then((data) => {
-                // just an example of destructuring
-                const dataJson = JSON.parse(data)
-                const { id, name, price, stock, author } = dataJson[0]
-                console.log("id: ", id)
-                console.log("name: ", name)
-                console.log("price: ", price)
+        return { success: true, data: data }
+      })
+      .catch((error) => {
+        return { success: false }
+      })
+  }
 
-                return { success: true, data: data }
-            })
-            .catch((error) => {
-                return { success: false }
-            })
-    }
-
-    //get one by id
-    async getOneById(id) {
-        return fsPromise
-            .readFile(path.join(__dirname, "..", "data", "manga.json"), {
-                encoding: "utf-8"
-            })
-            .then((data) => {
-                const findData = JSON.parse(data).find((element) => {
-                    return element.id === id
-                })
-                if (findData) {
-                    // console.log(findData)
-                    return { success: true, data: JSON.stringify(findData) }
-                }
-                else {
-                    return { success: false }
-                }
-            })
-            .catch((error) => {
-                return { success: false }
-            })
-    }
-
-
-    //add to the json file 
-    async add(body) {
-        try {
-            const data = await fsPromise.readFile(path.join(__dirname, "..", "data", "manga.json"), {
-                encoding: "utf-8"
-            })
-
-            const newManga = JSON.parse(body)
-
-
-            const jsonData = JSON.parse(data)
-            const lastId = jsonData.length > 0 ? jsonData[jsonData.length - 1].id : 0
-
-            const newData = {
-                id: lastId + 1,
-                ...newManga
+  async getByPriceAndStock(stock, price) {
+    // Reading the manga.json file
+    return (
+      fsPromise
+        .readFile(path.join(__dirname, "..", "data", "manga.json"), {
+          encoding: "utf-8",
+        })
+        // Promise resolve
+        .then((data) => {
+          // Looking for the object in manga.json
+          const findData = JSON.parse(data).filter((element) => {
+            return element.stock === stock && element.price === price
+          })
+          // If id is found --
+          if (findData.length > 0) {
+            return {
+              success: true,
+              message: "Successfully found data!",
+              data: JSON.stringify(findData)
             }
-            jsonData.push(newData)
+          }
+          // If id is not found --
+          else {
+            return {
+              success: false,
+              message: "Please enter a valid id!",
+            }
+          }
+        })
+        // Promise reject
+        .catch((error) => {
+          return {
+            success: false,
+            message: "Internal server error!",
+          }
+        })
+    )
+  }
 
-            await fsPromise.writeFile(
-                path.join(__dirname, "..", "data", "manga.json"),
-                JSON.stringify(jsonData),
-                { encoding: "utf-8" }
-            )
+  // Get one by id
+  async getOneById(id) {
+    // Reading the manga.json file
+    return (
+      fsPromise
+        .readFile(path.join(__dirname, "..", "data", "manga.json"), {
+          encoding: "utf-8",
+        })
+        // Promise resolve
+        .then((data) => {
+          // Looking for the id in manga.json
+          const findData = JSON.parse(data).find((element) => {
+            return element.id === id
+          })
+          // If id is found --
+          if (findData) {
+            return {
+              success: true,
+              message: "Successfully found data!",
+              data: JSON.stringify(findData),
+            }
+          }
+          // If id is not found --
+          else {
+            return {
+              success: false,
+              message: "Please enter a valid id!",
+            }
+          }
+        })
+        // Promise reject
+        .catch((error) => {
+          return {
+            success: false,
+            message: "Internal server error!",
+          }
+        })
+    )
+  }
 
-            return { success: true, message: "Manga added successfully" }
-        } catch (error) {
-            return { success: false, message: "Error reading file or parsing JSON" }
+  // Delete one by id
+  async deleteOneById(id) {
+    // Reading the file
+    return fsPromise
+      .readFile(path.join(__dirname, "..", "data", "manga.json"), {
+        encoding: "utf-8",
+      })
+      .then(async (data) => {
+        const jsonData = JSON.parse(data)
+        const dataToKeep = jsonData.filter((element) => element.id !== id)
+
+        if (dataToKeep.length < jsonData.length) {
+          await fsPromise.writeFile(
+            path.join(__dirname, "..", "data", "manga.json"),
+            JSON.stringify(dataToKeep),
+            { encoding: "utf-8" }
+          )
+
+          // Append timestamp to the log file
+          await fsPromise.appendFile(
+            path.join(__dirname, "..", "data", "timestamp_log.log"),
+            "Data deleted: " + (await this.getTimeFunc()) + "\n",
+            { encoding: "utf-8" }
+          )
+          return {
+            success: true,
+            message: "Successfully deleted data!",
+            data: JSON.stringify(dataToKeep),
+          }
+        } else {
+          return {
+            success: false,
+            message: "ID not found!",
+          }
         }
+      })
+      .catch((error) => {
+        return { success: false, message: "Internal server error!" }
+      })
+  }
+
+  // Validation fucntion for adding and update
+  async validation(mangaObj) {
+    const { name, price, stock, author } = JSON.parse(mangaObj)
+    const errors = {}
+    if (!name || name === "") {
+      errors.name = "Name is not provided"
     }
+    if (!price || price === "" || price > 1000) {
+      errors.price =
+        "Price should be provided, and it should be less than 1000."
+    }
+    if (!stock || stock === "" || stock === 0) {
+      errors.stock =
+        "Stock should be provided, and it should be greater than 0."
+    }
+    if (!author || author === "" || author === "Unknown") {
+      errors.author =
+        "Author name should be provided, and it should not be unknown."
+    }
+    if (Object.keys(errors).length > 0) {
+      return { success: false, errors: errors }
+    }
+  }
+
+  // Update one by id
+  async updateOneById(id, updatedManga) {
+    // Reading the file
+    const addValidation = await this.validation(JSON.stringify(updatedManga))
+    console.log(addValidation)
+    if (
+      addValidation &&
+      addValidation.success === false &&
+      addValidation.errors
+    ) {
+      return { success: false, message: addValidation.errors } // Return the validation errors if they exist
+    }
+    const data = await fsPromise.readFile(
+      path.join(__dirname, "..", "data", "manga.json"),
+      {
+        encoding: "utf-8",
+      }
+    )
+    const updateManga = JSON.parse(data)
+    const idx = updateManga.findIndex((element) => element.id == id)
+    if (idx >= 0) {
+      updateManga[idx] = { ...updateManga[idx], ...updatedManga }
+      console.log(updateManga[idx])
+
+      // Write in the file
+      await fsPromise.writeFile(
+        path.join(__dirname, "..", "data", "manga.json"),
+        JSON.stringify(updateManga),
+        { encoding: "utf-8" }
+      )
+      // Append timestamp to the log file
+      await fsPromise.appendFile(
+        path.join(__dirname, "..", "data", "timestamp_log.log"),
+        "Data updated: " + (await this.getTimeFunc()) + "\n",
+        { encoding: "utf-8" }
+      )
+      return { success: true, message: "Manga updated successfully" }
+    } else {
+      return { success: false, message: "Enter a valid ID" }
+    }
+  }
+
+  //add to the json file
+  async add(body) {
+    try {
+      const addValidation = await this.validation(body)
+      console.log(addValidation)
+      if (
+        addValidation &&
+        addValidation.success === false &&
+        addValidation.errors
+      ) {
+        return { success: false, message: addValidation.errors } // Return the validation errors if they exist
+      }
+      // Read the file
+      const data = await fsPromise.readFile(
+        path.join(__dirname, "..", "data", "manga.json"),
+        {
+          encoding: "utf-8",
+        }
+      )
+
+      if (data) {
+        const newManga = JSON.parse(body)
+
+        const jsonData = JSON.parse(data)
+        const lastId =
+          jsonData.length > 0 ? jsonData[jsonData.length - 1].id : 0
+
+        const newData = {
+          id: lastId + 1,
+          ...newManga,
+        }
+        jsonData.push(newData)
+
+        // Write in the file
+        await fsPromise.writeFile(
+          path.join(__dirname, "..", "data", "manga.json"),
+          JSON.stringify(jsonData),
+          { encoding: "utf-8" }
+        )
+
+        // Append timestamp to the log file
+        await fsPromise.appendFile(
+          path.join(__dirname, "..", "data", "timestamp_log.log"),
+          "Data added: " + (await this.getTimeFunc()) + "\n",
+          { encoding: "utf-8" }
+        )
+        return { success: true, message: "Manga added successfully" }
+      } else {
+        return { success: false, message: "Data not found!" }
+      }
+    } catch (error) {
+      return { success: false, message: "Error reading file or parsing JSON" }
+    }
+  }
+
+  //sort data by id (ascending order)
+  async sortDataById() {
+    try {
+      const data = await fsPromise.readFile(
+        path.join(__dirname, "..", "data", "manga.json"),
+        { encoding: "utf-8" }
+      )
+
+      const jsonData = JSON.parse(data)
+
+      jsonData.sort((a, b) => {
+        const idA = a.id
+        const idB = b.id
+
+        if (idA < idB) {
+          return -1
+        }
+        if (idA > idB) {
+          return 1
+        }
+        return 0
+      })
+
+      await fsPromise.writeFile(
+        path.join(__dirname, "..", "data", "manga.json"),
+        JSON.stringify(jsonData),
+        { encoding: "utf-8" }
+      )
+
+      // Append timestamp to the log file
+      await fsPromise.appendFile(
+        path.join(__dirname, "..", "data", "timestamp_log.log"),
+        "Data sorted by id: " + (await this.getTimeFunc()) + "\n",
+        { encoding: "utf-8" }
+      )
+
+      return { success: true, message: "Data sorted successfully" }
+    } catch (error) {
+      return { success: false, message: "Error sorting data" }
+    }
+  }
+
+  //sort data by name (descending order)
+  async sortDataByName() {
+    try {
+      const data = await fsPromise.readFile(
+        path.join(__dirname, "..", "data", "manga.json"),
+        { encoding: "utf-8" }
+      )
+
+      const jsonData = JSON.parse(data)
+
+      jsonData.sort((a, b) => {
+        const nameA = a.name.toUpperCase()
+        const nameB = b.name.toUpperCase()
+
+        if (nameA < nameB) {
+          return 1
+        }
+        if (nameA > nameB) {
+          return -1
+        }
+        return 0
+      })
+
+      await fsPromise.writeFile(
+        path.join(__dirname, "..", "data", "manga.json"),
+        JSON.stringify(jsonData),
+        { encoding: "utf-8" }
+      )
+
+      // Append timestamp to the log file
+      await fsPromise.appendFile(
+        path.join(__dirname, "..", "data", "timestamp_log.log"),
+        "Data sorted by name: " + (await this.getTimeFunc()) + "\n",
+        { encoding: "utf-8" }
+      )
+
+      return { success: true, message: "Data sorted successfully" }
+    } catch (error) {
+      return { success: false, message: "Error sorting data" }
+    }
+  }
 }
 
 module.exports = new Product()
